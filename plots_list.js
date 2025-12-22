50def2be76b56496fa3f0b2ff1ee76a72192ac9d
Kobel Cyril Sean (ck18y530) <ck18y530@sedna01.ubelix.unibe.ch>
2025-12-22 12:23:01 +0100

    Auto update plots 2025-12-22 12:23

--- plots_list.js
+++ plots_list.js
@@ -1,4 +1,7 @@
 const plots = [
+    "deleted_files.png",
+    "gpsxtr_stats.png",
+    "large_obs_files.png",
     "num_obs_files.png",
     "Orbit_Misclosures_per_Satellite.png",
     "test_final.png",
