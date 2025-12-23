41b4e072797f3e5b96abff7c64950bea54530262
Kobel Cyril Sean (ck18y530) <ck18y530@sedna01.ubelix.unibe.ch>
2025-12-23 12:55:01 +0100

    Auto update plots 2025-12-23 12:55

--- comparisons_list.js
+++ comparisons_list.js
@@ -1,9 +1,13 @@
 const comparison_plots = [
+    "clk_comparison.png",
+    "crd_comparison.png",
     "CRD.png",
     "erp_comparison.png",
     "ERP.png",
+    "helmert_F1I_F1.png",
     "HELMERT_F1I.png",
     "HELMERT_F1.png",
+    "pre_comparison.png",
     "PRE.png",
     "std_comparison.png",
     "STD.png",
