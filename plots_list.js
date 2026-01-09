a15d8bac906c7910a591aa260175bd96b711ab11
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-01-09 04:12:14 +0100

    Auto update plots 2026-01-09 04:12

--- plots_list.js
+++ plots_list.js
@@ -1,10 +1,19 @@
 const latest_plots = [
     "amb_stats.png",
+    "amb_stats.png",
+    "deleted_files.png",
     "deleted_files.png",
     "gpsxtr_obs.png",
+    "gpsxtr_obs.png",
+    "gpsxtr_par.png",
     "gpsxtr_par.png",
     "gpsxtr_rms.png",
+    "gpsxtr_rms.png",
+    "large_obs_files.png",
     "large_obs_files.png",
     "num_obs_files.png",
+    "num_obs_files.png",
+    "obsxtr_bad.png",
+];
     "obsxtr_bad.png",
 ];
