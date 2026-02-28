52310a2c3bbd9ad57c4e1aa9fd034c76640aa93d
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-02-28 10:54:01 +0100

    Auto update plots + satclk 2026-02-28 10:54

--- plots_list.js
+++ plots_list.js
@@ -1,25 +1,10 @@
 const latest_plots = [
-    "gpsxtr_par.png",
-    "amb_stats.png",
-    "gpsxtr_rms.png",
     "amb_stats.png",
     "clock_completeness_heatmap.png",
-    "large_obs_files.png",
-    "clock_completeness_heatmap.png",
-    "gpsxtr_obs.png",
-    "num_obs_files.png",
     "gpsxtr_obs.png",
     "gpsxtr_par.png",
-    "obsxtr_bad.png",
-    "gpsxtr_par.png",
-];
-    "gpsxtr_rms.png",
     "gpsxtr_rms.png",
     "large_obs_files.png",
-    "large_obs_files.png",
     "num_obs_files.png",
-    "num_obs_files.png",
-    "obsxtr_bad.png",
-];
     "obsxtr_bad.png",
 ];
