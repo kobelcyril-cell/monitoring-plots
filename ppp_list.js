77ce62be04bf2eb021be63f55f69fbd503304690
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-07-02 09:25:07 +0200

    Auto update plots + satclk 2026-07-02 09:25

--- ppp_list.js
+++ ppp_list.js
@@ -1,28 +1,7 @@
 const ppp_plots = [
-    "ROTH_plot.png",
-    "ROTH_plot.png",
-    "ROTH_plot.png",
     "CPVG_plot.png",
-    "CPVG_plot.png",
-    "SCOR_plot.png",
-    "SCOR_plot.png",
-    "SCOR_plot.png",
     "ROTH_plot.png",
-    "ROTH_plot.png",
-    "WGTN_plot.png",
-    "WGTN_plot.png",
-    "WGTN_plot.png",
     "SCOR_plot.png",
-    "SCOR_plot.png",
-    "ZIM3_plot.png",
-];
-    "ZIM3_plot.png",
-];
     "WGTN_plot.png",
-    "ZIM3_plot.png",
-];
-    "WGTN_plot.png",
-    "ZIM3_plot.png",
-];
     "ZIM3_plot.png",
 ];
