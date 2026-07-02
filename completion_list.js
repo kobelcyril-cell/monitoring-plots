486d4af3414288378982c1eacef0642a48626769
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-07-02 09:17:38 +0200

    Auto update plots + satclk 2026-07-02 09:17

--- completion_list.js
+++ completion_list.js
@@ -1,3 +1,11 @@
 const completion_plots = [
     "process_completion.png",
 ];
+    "process_completion.png",
+    "process_completion.png",
+];
+    "process_completion.png",
+    "process_completion.png",
+];
+];
+];
--- runtime_list.js
+++ runtime_list.js
@@ -1,4 +1,16 @@
 const runtime_plots = [
     "runtime_comparison.png",
+    "runtime_comparison.png",
+    "runtime_comparison.png",
+    "runtime_comparison.png",
+    "runtime_comparison.png",
     "runtime_solutions.png",
+];
+    "runtime_solutions.png",
+    "runtime_solutions.png",
+    "runtime_solutions.png",
+    "runtime_solutions.png",
+];
+];
+];
 ];
