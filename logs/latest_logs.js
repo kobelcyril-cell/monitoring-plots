ff6775ee735f528e7b1ae7005951254c982a327f
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-07-08 21:00:08 +0200

    Auto update plots + satclk 2026-07-08 21:00

--- logs/latest_logs.js
+++ logs/latest_logs.js
@@ -1,6 +1,6 @@
 const latest_logs = [
-    "PROCESSING_26179",
-    "PROCESSING_26180",
-    "PROCESSING_26181",
     "PROCESSING_26182",
+    "PROCESSING_26183",
+    "PROCESSING_26184",
+    "PROCESSING_26185",
 ];
