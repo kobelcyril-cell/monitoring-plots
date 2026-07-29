935aeae2e619a296b14f57bfbede6cf7e315d926
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-07-29 15:37:12 +0200

    Auto update plots + satclk 2026-07-29 15:37

--- satclk_list.js
+++ satclk_list.js
@@ -203,4 +203,5 @@ const satclk_files = [
     "sat_clock_completeness_26202.txt",
     "sat_clock_completeness_26203.txt",
     "sat_clock_completeness_26204.txt",
+    "sat_clock_completeness_26205.txt",
 ];
