43f3d45f5962789ab14d1eb13e5bf42beb157af4
Kobel Cyril Sean <cyril.kobel@unibe.ch>
2026-08-22 00:49:16 +0200

    Auto update plots + satclk 2026-08-22 00:49

--- comparisons_list.js
+++ comparisons_list.js
@@ -1,6 +1,11 @@
 const comparison_plots = [
-    "crd_comparison.png",
     "erp_comparison.png",
+    "helmert_F1I_F1.png",
+    "crd_comparison.png",
+    "pre_comparison.png",
+    "erp_comparison.png",
+    "std_comparison.png",
+];
     "helmert_F1I_F1.png",
     "pre_comparison.png",
     "std_comparison.png",
