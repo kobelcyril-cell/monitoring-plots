0c098dccaa7cf8766ad0bc3ef8a9f09845a6f054
CYRIL KOBEL <kobelcyril@gmail.com>
2026-09-20 04:42:03 +0200

    Auto update plots + satclk 2026-09-20 04:42

--- satclk_list.js
+++ satclk_list.js
@@ -243,4 +243,5 @@ const satclk_files = [
     "sat_clock_completeness_26255.txt",
     "sat_clock_completeness_26256.txt",
     "sat_clock_completeness_26257.txt",
+    "sat_clock_completeness_26258.txt",
 ];
